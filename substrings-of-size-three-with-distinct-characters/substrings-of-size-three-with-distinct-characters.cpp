0f95a119f9009a2adf3da1ed49240b63900beed0
Saptarshi Mandal <79189686+saptarshiweb@users.noreply.github.com>
2021-09-05 09:24:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (95.61%) - LeetHub

---
+++ substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        
+        if(s.length()<3)
+        {
+            return 0;
+        }
+        int count=0;
+        
+        for(int i=0;i<s.length()-2;i++)
+        {
+            
+            if((s[i]!=s[i+1])&&(s[i]!=s[i+2])&&(s[i+1]!=s[i+2]))
+                count++;
+            
+        }
+        
+        return count;
+        
+    }
+};
