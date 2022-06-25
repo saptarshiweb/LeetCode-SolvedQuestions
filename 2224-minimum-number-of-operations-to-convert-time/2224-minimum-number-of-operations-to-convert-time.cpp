ae2d113cb3b267acdae41b3c62fc641f470cffc1
Saptarshi Mandal <79189686+saptarshiweb@users.noreply.github.com>
2022-06-25 09:14:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (60.77%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    int dp[3000][10];
-    
-    int solve(vector<int> &arr, int sum, int index){
-        if(sum==0)  
-            return 0;
-        
-        if(index<0 || sum<0 )  
-            return INT_MAX-1;
-        
-        if(dp[sum][index]!=-1)   
-            return dp[sum][index];
-        
-        
-        return dp[sum][index]=min(1+solve(arr, sum-arr[index],index), solve(arr, sum, index-1));
-    }
     int convertTime(string current, string correct)
     {
         int h1=stoi(current.substr(0,2));
@@ -23,14 +8,18 @@ public:
         int h2=stoi(correct.substr(0,2));
         int m2=stoi(correct.substr(3,2));
         
-        int diff=(m2-m1);
+        int diff=m2-m1;
         
         diff+=(h2-h1)*60;
         
-        memset(dp,-1,sizeof(dp));
-        vector<int> arr({1,5,15,60});
-        int ans=solve(arr,diff,3);
-        
+        int ans=0;
+        ans+=diff/60;
+        diff=diff%60;
+        ans+=diff/15;
+        diff=diff%15;
+        ans+=diff/5;
+        diff=diff%5;
+        ans+=diff/1;
         return ans;
         
     }
