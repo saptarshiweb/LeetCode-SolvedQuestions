4aed37dbbada2fcf0815959ede2fc6192120337f
Saptarshi Mandal <79189686+saptarshiweb@users.noreply.github.com>
2022-06-28 20:40:20 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (74.91%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removePalindromeSub(string s)
+    {
+        if(s=="")
+            return 0;
+        int j=s.size()-1;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!=s[j])
+                return 2;
+            j--;
+        }
+        return 1;
+        
+    }
+};
