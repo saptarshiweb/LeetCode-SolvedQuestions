6cb765bad132cd213e3850c3b386e17c272d3ab0
Saptarshi Mandal <79189686+saptarshiweb@users.noreply.github.com>
2022-07-19 11:07:28 +0530

    Time: 17 ms (39.91%), Space: 10.3 MB (6.02%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-       int rows=grid.size();
-        int columns=grid[0].size();
-        int dp[rows][columns];
-        int i=1,j=1;
-        dp[0][0]=grid[0][0];
-        while(i<rows)
-        {
-            dp[i][0]=grid[i][0]+dp[i-1][0];
-            i++;
+    int dp[400][400];
+    int getMinPath(vector <vector <int>> &grid, int i, int j) {
+        if (i == 0 && j == 0) {
+            return grid[i][j];
         }
-        while(j<columns)
-        {
-            dp[0][j]=grid[0][j]+dp[0][j-1];
-            j++;
-        }
-        for(int i=1;i<rows;i++)
-        {
-            for(int j=1;j<columns;j++)
-            {
-                dp[i][j]=grid[i][j]+ min(dp[i-1][j],dp[i][j-1]);
-            }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
         
-        return dp[rows-1][columns-1];
+        int m = INT_MAX;
+        if (j > 0) {
+            m = grid[i][j] + getMinPath(grid, i, j - 1);
+        }
+        if (i > 0) {
+            int s = grid[i][j] + getMinPath(grid, i - 1, j);
+            m = min(m, s);
+        }
+        
+        dp[i][j] = m;
+        return dp[i][j];
+    }
+    
+    int minPathSum(vector<vector<int>>& grid)
+    {
+        memset(dp,-1,sizeof(dp));
+        
+        return getMinPath(grid,grid.size()-1,grid[0].size()-1);
+      
     }
 };
